48c455d6bbffe35f2a21530b0e63cf2376ffaa11
Joyjeet <joyxbhp6@gmail.com>
2024-02-12 12:22:20 +0530

    First Commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signUp = (req, res) => {
+    console.log("SignUp User");
+}
+export const login = (req, res) => {
+    console.log("Login User");
+}
+
+export const logout = (req, res) => {
+    console.log("Logout User");
+}
+
