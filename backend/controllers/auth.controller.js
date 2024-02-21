a989c654e21fc095e7fe3acf0781aa04f24e47b4
Joyjeet Mukherjee <joyxbhp6@gmail.com>
2024-02-22 01:33:11 +0530

    Database and User model created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,56 @@
-export const signUp = (req, res) => {
-    console.log("SignUp User");
-}
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+
+export const signUp = async (req, res) => {
+  try {
+    const { fullName, userName, gender, password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
+    }
+
+    const user = await User.findOne({ userName });
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+    // HASH PASSWORD HERE
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt)
+
+    //  Avatar placeholder website for API
+    // https://avatar-placeholder.iran.liara.run/
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
+
+    const newUser = new User({
+      fullName,
+      password: hashPassword,
+      userName,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      userName: newUser.userName,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in signup Controller", error.message);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+};
 export const login = (req, res) => {
-    console.log("Login User");
-}
+  console.log("Login User");
+};
 
 export const logout = (req, res) => {
-    console.log("Logout User");
-}
-
+  console.log("Logout User");
+};
--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,27 @@
 import express from 'express';
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToDB from './db/connectToDB.js';
 
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    // root route http://localhost:5000/
-    res.send("Hello World!!");
-})
-
+app.use(express.json())    // To parse the incoming requests with json payloads (from req.body)
 
 app.use("/api/auth", authRoutes)
 
+// app.get("/", (req, res) => {
+//     // root route http://localhost:5000/
+//     res.send("Hello World!!");
+// })
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
+
+app.listen(PORT, () => {
+    connectToDB();    
+    console.log(`Server running on port ${PORT}`)
+
+})
