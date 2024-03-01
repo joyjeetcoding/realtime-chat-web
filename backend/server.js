2a72fa14bc249d7afe92ca8dc4b8bddf8d33712c
Joyjeet <joyxbhp6@gmail.com>
2024-03-01 22:06:26 +0530

    Full completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 // Before writing "import" mention "type": "module" in package.json file
+import path from "path"
 import express from 'express';
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
+
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -14,6 +16,9 @@ import { app, server } from './socket/socket.js';
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+// Before deploying write this code
+const __dirname = path.resolve();
+
 app.use(express.json())    // To parse the incoming requests with json payloads (from req.body)
 app.use(cookieParser());    // To parse the incoming cookie from req.cookies
 
@@ -21,6 +26,17 @@ app.use(cookieParser());    // To parse the incoming cookie from req.cookies
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
+
+
+// Before deploying write this code
+// This middleware will be served as static files
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+// When you will run npm run build under the folder frontend... it will create a folder named dist
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 // app.get("/", (req, res) => {
 //     // root route http://localhost:5000/
