964a83b681cca18ea217c19346c083df8113e425
Joyjeet <joyxbhp6@gmail.com>
2024-03-19 10:51:49 +0530

    Some changes

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
   const [userName, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const {loading, login} = useLogin();
+  const { loading, login } = useLogin();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,16 +17,33 @@ const Login = () => {
   console.log(password);
 
   return (
-    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+    <div className="text-white flex flex-col items-center justify-center relative h-screen w-full">
+      <div className="absolute top-1/2 -translate-x-1/2 -translate-y-1/2 left-1/2 flex justify-center items-center flex-col backdrop-filter backdrop-blur-lg bg-opacity-0 p-4 rounded-2xl">
         <h1 className="text-3xl font-semibold text-center text-gray-300">
-          Login
-          <span className="text-blue-500">ChatApp</span>
+          Login 
         </h1>
 
         <form onSubmit={handleSubmit}>
-          <div>
-            <label className="label p-2">
+          <div className="py-2">
+            <label className="input input-bordered flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
+              >
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+              </svg>
+              <input
+                type="text"
+                className="grow"
+                value={userName}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+              />
+            </label>
+
+            {/* <label className="label p-2">
               <span className="text-base label-text">Username</span>
             </label>
             <input
@@ -35,10 +52,27 @@ const Login = () => {
               className="w-full input input-bordered h-10"
               value={userName}
               onChange={(e) => setUsername(e.target.value)}
-            />
+            /> */}
           </div>
           <div>
-            <label className="label">
+            <label className="input input-bordered flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input type="password" className="grow" value={password}
+              onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+            </label>
+
+            {/* <label className="label">
               <span className="text-base label-text">Password</span>
             </label>
             <input
@@ -47,7 +81,7 @@ const Login = () => {
               className="w-full input input-bordered h-10"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-            />
+            /> */}
           </div>
           <Link
             to="/signup"
@@ -58,9 +92,11 @@ const Login = () => {
 
           <div>
             <button disabled={loading} className="btn btn-block btn-sm mt-2">
-              {
-                loading ? <span className="loading loading-spinner"></span> : "Login"
-              }
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Login"
+              )}
             </button>
           </div>
         </form>
